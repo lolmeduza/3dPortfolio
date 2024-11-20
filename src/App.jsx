17976f3c86814f21645fbc303634ea39e1252439
Andrey <lolmeduza997@yandex.ru>
2024-11-20 17:46:10 +0300

    first commit with bird, plane and island

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Home, About, Contact, Projects } from "./components/pages";
+import Navbar from "./components/Navbar";
+const App = () => {
+  return (
+    <main className="bg-slate-300/20">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/projects" element={<Projects />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+        </Routes>
+      </Router>
+    </main>
+  );
+};
+
+export default App;
