2a8413ab6a2e9ff8e140c12af6c9f11ebb6cdcc6
Andrey <lolmeduza997@yandex.ru>
2024-11-21 16:18:43 +0300

    vitex2

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ["**/*.glb"],
-  base: "/https://lolmeduza.github.io/3dPortfolio/",
+  base: "/",
 });
